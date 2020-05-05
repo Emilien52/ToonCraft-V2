196c8a42cabcddbcc945a56efcc30fb28aa8c229
Emilien52 <40137638+Emilien52@users.noreply.github.com>
2020-05-06 00:24:26 +0200

    Delete version_distant.php

--- version_distant.php
+++
@@ -1,6 +0,0 @@
-<?php
-$versionthemerelease = file_get_contents('http://angrykiller.raidghost.com/vercheck/tooncraft/version.txt');
-?>
-<?php
-$descriptionupdate = file_get_contents('http://angrykiller.raidghost.com/vercheck/tooncraft/description.txt');
-?>
